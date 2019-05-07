105077d71a60a0e92664eff8b249dfb438795b6d
rendzso <bodnarreno@gmail.com>
2019-05-07 23:57:10 +0200

    worker functions added

--- src/backend/Worker/ShutterWorkerController.js
+++ src/backend/Worker/ShutterWorkerController.js
@@ -10,4 +10,8 @@ router.post('/select', async (req, res) => {
     res.status(200).send(await srs.selectOrder(req.body))
 })
 
+router.post('/success', async (req, res) => {
+    res.status(200).send(await srs.successOrder(req.body))
+})
+
 module.exports = router;
--- src/backend/Worker/ShutterWorkerService.js
+++ src/backend/Worker/ShutterWorkerService.js
@@ -2,25 +2,43 @@ var srs = require('../ShutterDAO')
 const collection = 'Orders'
 
 async function readOrders() {
-    const all = await srs.readAll(collection, {"submitted": "yes"})
-    const items = all[0]["items"]
-    let result = []
-    for(let entity of items){
-        if(entity["worker"] === "none"){
-            entity["customerID"] = all.customerID
-            result.push(entity)
+    const all = await srs.readAll(collection, {"submitted": "submitted"})
+    if (all.length !== 0) {
+        let result = []
+        for (let entity of all) {
+            if (entity["items"].length !== 0) {
+                for (let item of entity["items"]) {
+                    if (item["worker"] === "none") {
+                        const filtered = {};
+                        filtered["customerID"] = entity["customerID"];
+                        filtered["orderID"] = entity["orderID"];
+                        filtered["items"] = item;
+                        result.push(filtered)
+                    }
+                }
+            }
         }
+        return(result)
     }
-    console.log(result)
+}
+
+async function checkEverything(customerID, orderID){
+    const all = await srs.readAll(collection, {"submitted": "submitted"})
 
 }
 
 async function selectOrder(data) {
-    const where = {"customerID": data.customerID, "windowID": data.windowID}
-    srs.updateOne(collection, where, {$set: {"selected" : data.workerID}})
+    const where = {"customerID": data.customerID, "orderID": data.orderID, "items.itemID" : data.itemID}
+    srs.updateOne(collection, where, {$set: {"items.$.worker": data.worker, "items.$.shutterStatus" : "inProgress"}})
+}
+
+async function successOrder(data) {
+    const where = {"customerID": data.customerID, "orderID": data.orderID, "items.itemID" : data.itemID}
+    srs.updateOne(collection, where, {$set: {"items.$.worker": data.worker, "items.$.shutterStatus" : "inProgress"}})
 }
 
 module.exports = {
     "readReady": readOrders,
-    "selectOrder": selectOrder
+    "selectOrder": selectOrder,
+    "successOrder" : successOrder
 }
