bf1fed98974fecdddcc8aa5a358f686c537dfdff
rendzso <bodnarreno@gmail.com>
2019-05-08 01:19:06 +0200

    worker functions added, basicly working

--- src/backend/Worker/ShutterWorkerService.js
+++ src/backend/Worker/ShutterWorkerService.js
@@ -18,27 +18,43 @@ async function readOrders() {
                 }
             }
         }
-        return(result)
+        return (result)
     }
 }
 
-async function checkEverything(customerID, orderID){
-    const all = await srs.readAll(collection, {"submitted": "submitted"})
+async function checkEverything(customerID, orderID) {
+    const data = await srs.readWithData(collection, {"customerID": customerID, "orderID": orderID})
+    let all = 0
+    let ready = 0
+    console.log(data)
+    for (let entity of data[0]["items"]) {
+        all += 1
+        if (entity["shutterStatus"] === "success") {
+            ready += 1
+        }
+    }
 
+    console.log("all: " + all)
+    console.log("ready: " + ready)
+    if(all === ready){
+        console.log("The whole order is ready.")
+        srs.updateOne(collection, {"customerID": customerID, "orderID": orderID}, {$set: {"status": "creatingReceipt"}})
+    }
 }
 
 async function selectOrder(data) {
-    const where = {"customerID": data.customerID, "orderID": data.orderID, "items.itemID" : data.itemID}
-    srs.updateOne(collection, where, {$set: {"items.$.worker": data.worker, "items.$.shutterStatus" : "inProgress"}})
+    const where = {"customerID": data.customerID, "orderID": data.orderID, "items.itemID": data.itemID}
+    srs.updateOne(collection, where, {$set: {"items.$.worker": data.worker, "items.$.shutterStatus": "inProgress"}})
 }
 
 async function successOrder(data) {
-    const where = {"customerID": data.customerID, "orderID": data.orderID, "items.itemID" : data.itemID}
-    srs.updateOne(collection, where, {$set: {"items.$.worker": data.worker, "items.$.shutterStatus" : "inProgress"}})
+    const where = {"customerID": data.customerID, "orderID": data.orderID, "items.itemID": data.itemID}
+    await srs.updateOne(collection, where, {$set: {"items.$.shutterStatus": "success"}})
+    checkEverything(data.customerID, data.orderID)
 }
 
 module.exports = {
     "readReady": readOrders,
     "selectOrder": selectOrder,
-    "successOrder" : successOrder
+    "successOrder": successOrder
 }
