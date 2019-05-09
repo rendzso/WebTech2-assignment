df5e239276faaba88ec98b61c26d35de69d2609d
rendzso <bodnarreno@gmail.com>
2019-05-09 19:34:50 +0200

    trying add frontend

--- src/backend/server.js
+++ src/backend/server.js
@@ -12,7 +12,7 @@ app.use('/customer', CustomerController);
 
 app.use('/worker', WorkerController);
 
-app.use('/manager', ManagerController)
+app.use('/manager', ManagerController);
 
 app.listen(8080, () => {
     console.log('Server is listening on 8080!');
