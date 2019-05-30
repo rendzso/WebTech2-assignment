71822ea75f6d88af45f8a1a042e3ce5c5c10a2bd
rendzso <bodnarreno@gmail.com>
2019-05-30 14:53:34 +0200

    build added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import CustomerActions from "./actions/CustomerActions"
 import WorkerActions from "./actions/WorkerActions";
 import ManagerActions from "./actions/ManagerActions";
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path')
 
 app.use(bodyParser.json());
 
@@ -8,13 +9,18 @@ const CustomerController = require('./controller/ShutterCustomerController');
 const WorkerController = require('./controller/ShutterWorkerController');
 const ManagerController = require('./controller/ShutterManagerController');
 
+app.use(express.static(path.join(__dirname, './client/build')))
+
+const index = require('./controller/index')
+app.use('/', index)
+
 app.use('/customer', CustomerController);
 
 app.use('/worker', WorkerController);
 
 app.use('/manager', ManagerController)
 
-app.listen(8081, () => {
+app.listen(8080, () => {
     console.log('Server is listening on 8080!');
 });
 
